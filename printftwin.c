11f9c7a8252de32c568afde48ddb625a7e8f13ff
TrudyB-T <drtrudybannerman.com>
2023-05-27 16:58:55 -0700

    printf

---
+++ printftwin.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * _printf -  produces output according to a format
+ * @format: character string
+ *
+ *
+ * Return: the number of characters printed
+ * (excluding the null byte used to end output to strings)
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list p;
+
+	va_start(p, format);
+
+	int y = 0;
+	char i;
+	char *k;
+
+	if (format != NULL)
+	{
+		for (; format != '\0'; format++)
+		{
+			if (format == '%')
+				format++;
+
+			{
+				if (format == 'c')
+				{
+					i = (char)va_arg(p, int)
+						_putchar(i);
+						y++;
+				}
+				if (format == "s")
+				{
+					k = va_arg(p, char *);
+`
+					while (k != '\0')
+					{
+						_putchar(k);
+						k++;
+						y++;
+					}
+				}
+			}
+		}
+	}
+	va_end(p);
+	return (y);
+}
