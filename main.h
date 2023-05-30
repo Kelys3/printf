65e116b046547484fb5977f19730ddfc52cf5969
TrudyB-T <drtrudybannerman.com>
2023-05-30 12:28:22 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+int print_char(va_list l);
+int print_string(va_list l);
+
+#endif
